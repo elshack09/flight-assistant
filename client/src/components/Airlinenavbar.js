cc17dbbfb8c56caed0cb28cd68deef3d1ecfb114
elshack09 <elshack09@gmail.com>
2018-07-02 15:10:16 -0400

    deleted  heroku clone folder

--- client/src/components/Airlinenavbar.js
+++ client/src/components/Airlinenavbar.js
@@ -1,5 +1,14 @@
-import React from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Button} from 'reactstrap';
+import React from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Button
+} from "reactstrap";
 
 export default class Airlinenavbar extends React.Component {
   constructor(props) {
@@ -7,7 +16,7 @@ export default class Airlinenavbar extends React.Component {
 
     this.toggleNavbar = this.toggleNavbar.bind(this);
     this.state = {
-      collapsed:true
+      collapsed: true
     };
   }
 
@@ -20,21 +29,29 @@ export default class Airlinenavbar extends React.Component {
     return (
       <div>
         <Navbar color="faded" light>
-          <NavbarBrand href="/" className="mr-auto">Rosy Airlines</NavbarBrand>
-          <Button color="danger" size="lg" >Emergency</Button>
+          <NavbarBrand href="/" className="mr-auto">
+            Rosy Airlines
+          </NavbarBrand>
+          <Button color="danger" size="lg">
+            Emergency
+          </Button>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse isOpen={!this.state.collapsed} navbar>
             <Nav navbar>
               <NavItem>
-                <NavLink target="_blank" href="https://www.expedia.com/">Rosy Airlines Website</NavLink>
+                <NavLink target="_blank" href="https://www.expedia.com/">
+                  Rosy Airlines Website
+                </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink target="_blank" href="https://www.tsa.gov/">TSA Webesite</NavLink>
+                <NavLink target="_blank" href="https://www.tsa.gov/">
+                  TSA Webesite
+                </NavLink>
               </NavItem>
             </Nav>
           </Collapse>
         </Navbar>
       </div>
     );
-  }// test
+  } // test
 }
