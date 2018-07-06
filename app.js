8b0c44ecaf11080dd231c62ddd17aa42cbba5799
elshack09 <elshack09@gmail.com>
2018-07-05 22:40:10 -0400

    changed code in app.js

--- app.js
+++ app.js
@@ -16,12 +16,10 @@ connection.on("error", err => {
   console.log("Mongoose default connection error: " + err);
 });
 
-
-// requiring routes 
-const food = require('./routes/api/food');
-const users = require('./routes/api/users');
-
-const mood = require('./routes/api/mood')
+// requiring routes
+const food = require("./routes/api/food");
+const users = require("./routes/api/users");
+const mood = require("./routes/api/mood");
 
 //test
 // intializing the app
@@ -29,23 +27,22 @@ const app = express();
 
 app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// Routes
+app.use("/api/users", users);
+app.use("/api/food", food);
+app.use("/api/mood", mood);
 
 app.use(express.static(__dirname + "/client/build/"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client/build/index.html");
 });
 
+
 // //Index
-
-
-// Routes
-app.use('/api/users', users);
-app.use('/api/food', food);
-app.use('/api/mood', mood);
 
 
 
